ea8da1dd4e2cd7661132694760ff80142951d04a
gonzalemi <emiliano.gonzalez@hotmail.com>
2020-12-10 21:22:33 -0300

    fix if repetidos

--- data/script_creación_BI.sql
+++ data/script_creación_BI.sql
@@ -13,17 +13,11 @@ IF OBJECT_ID('LOS_GEDDES.Bi_Compras_automoviles', 'U') IS NOT NULL
 IF OBJECT_ID('LOS_GEDDES.Bi_Ventas_automoviles', 'U') IS NOT NULL
 	DROP TABLE LOS_GEDDES.Bi_Ventas_automoviles
 
-IF OBJECT_ID('LOS_GEDDES.Bi_Compras_autopartes', 'U') IS NOT NULL
-	DROP TABLE LOS_GEDDES.Bi_Compras_autopartes
-
 IF OBJECT_ID('LOS_GEDDES.Bi_Ventas_autopartes', 'U') IS NOT NULL
 	DROP TABLE LOS_GEDDES.Bi_Ventas_autopartes
 
 IF OBJECT_ID('LOS_GEDDES.Bi_Compras_autopartes', 'U') IS NOT NULL
 	DROP TABLE LOS_GEDDES.Bi_Compras_autopartes
-
-IF OBJECT_ID('LOS_GEDDES.Bi_Ventas_autopartes', 'U') IS NOT NULL
-	DROP TABLE LOS_GEDDES.Bi_Ventas_autopartes
 
 IF OBJECT_ID('LOS_GEDDES.Bi_Instantes', 'U') IS NOT NULL
 	DROP TABLE LOS_GEDDES.Bi_Instantes
